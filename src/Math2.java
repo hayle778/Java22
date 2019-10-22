aca448e7d7cf2ca7ac8e2306fa2ba09763f4fd03
hayle778 <hayjclove@gmail.com>
2019-10-22 10:18:50 -0400

    Java22

---
+++ src/Math2.java
@@ -0,0 +1,11 @@
+
+public class Math2 {
+    public static void main (String[]args){
+        int mynumber = 3;
+        double myothernumber = 1.745;
+        System.out.println("mynumb is" + mynumber);
+        System.out.println("the value of my other number");
+
+    }
+
+}
---
+++ src/Maths2.java
@@ -0,0 +1,14 @@
+
+public class Maths2 {
+    public static void main(String[] args) {
+
+        boolean mynumber = true;
+        int baby = 1;
+        double familysize  = 3;
+        System.out.println("mynumber" + mynumber);
+        System.out.println("int baby" );
+        System.out.println("familysize");
+
+
+    }
+}
